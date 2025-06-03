f1125308cee020717ba425b6c633859a0fc075f2
MasakiYoshida39 <149563224+MasakiYoshida39@users.noreply.github.com>
2025-06-03 17:37:16 +0900

    合計の式作成

--- my_file.py
+++ my_file.py
@@ -1,5 +1,18 @@
-# これはコメントです
 """
+キウイ 300円 3個
+リンゴ 1000円 2個
+ぶどう 6600円 4個
+合計を出力
+"""
+Kiwi = 300*3
+Apple = 1000*2
+Grapes = 6600*4
+Total = Grapes + Apple+ Kiwi
+print(Total)
+
+
+"""
+# これはコメントです
 #ターミナルでpython my_file.pyを行う
 # "青, 黒" という結果を生む式
 print("青、" + "黒"  )
@@ -11,8 +24,8 @@ int takasi = 10 * 5
  File "/Users/masa/my_project/my_file.py", line 9
     int takasi = 10 * 5         
         ^^^^^^
-SyntaxError: invalid syntax       
-"""  
+SyntaxError: invalid syntax    
+
 itihara = 4 * 8
 kasahara = 8 * 5
 meesi = 6 * 9
@@ -20,3 +33,6 @@ CR7 = 9 * 20
 honnda = 11 + 10
 
 print(CR7)
+
+"""  
+
