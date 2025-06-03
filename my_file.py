e04a8698165480ff744351d5933cba2e3f6cc1c1
MasakiYoshida39 <149563224+MasakiYoshida39@users.noreply.github.com>
2025-06-03 17:26:45 +0900

    代入式記載

--- my_file.py
+++ my_file.py
@@ -3,8 +3,20 @@
 #ターミナルでpython my_file.pyを行う
 # "青, 黒" という結果を生む式
 print("青、" + "黒"  )
-# 189 という結果を生む式
+# 189という結果を生む式
 print(94+95)
-"""
 
-print(94+95)
+int takasi = 10 * 5
+下記のエラー発生JavaやC言語みたいな記載禁止
+ File "/Users/masa/my_project/my_file.py", line 9
+    int takasi = 10 * 5         
+        ^^^^^^
+SyntaxError: invalid syntax       
+"""  
+itihara = 4 * 8
+kasahara = 8 * 5
+meesi = 6 * 9
+CR7 = 9 * 20
+honnda = 11 + 10
+
+print(CR7)
