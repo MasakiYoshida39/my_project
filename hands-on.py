074341ae27c15fdb989359df349d8cc14c12bb38
MasakiYoshida39 <yashukasono@gmail.com>
2025-06-17 16:36:07 +0900

    問題2-1完了

--- hands-on.py
+++ hands-on.py
@@ -1,4 +1,16 @@
+# 入力された文字列を変数に格納
+name = int(input("あなたの名前を入力してください: "))
+
+
+if name % 3 == 0:
+    print("foo")
+else:
+     print("noo")
+
+
+""" コメント(1-1)
 for i in range(1, 10):
     for j in range(1, 10):
         print(i * j, end= " ")
     print("「{}」の段です".format(i))
+""" 
