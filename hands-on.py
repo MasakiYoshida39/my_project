2110a44a1cfbd6067151c55d7c1ae301d72828a2
MasakiYoshida39 <yashukasono@gmail.com>
2025-06-17 17:09:20 +0900

    問題2-3OK

--- hands-on.py
+++ hands-on.py
@@ -1,13 +1,24 @@
+
+x = 0
+limit = 20000
+
+for i in range(2, limit + 1):
+    for j in range(2, (i // 2) + 1):
+        if i % j == 0:
+            break
+    else:
+        x += i
+print(x)
+
+
+
+""" コメント(2-2)
 h = int(5)
 for i in range(1, h + 1):
     for j in range(i):
         print("*", end="")
     print()
-
-
-
-
-
+"""
 
 """ コメント(2-1)
 i = int(input("iの数字を入力してください: "))
