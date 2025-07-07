1e0ee848409c49e63eb7d1e4203940f763d19802
MasakiYoshida39 <yashukasono@gmail.com>
2025-07-08 06:48:26 +0900

    Matplotlibの学習（折れ線グラフ作成）

---
+++ plt.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+x = np.linspace(-5, 5, 100) # -5から5までを100ステップに分けた配列を生成
+y = np.sin(x) # xの各値に対してsin関数の値を計算
+ 
+plt.plot(x, y) # 折れ線グラフをプロット
+plt.show() # グラフを表示
