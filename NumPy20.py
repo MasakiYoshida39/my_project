031bc1ba1eb680932e26e2d239c309c2142c2405
MasakiYoshida39 <yashukasono@gmail.com>
2025-07-09 21:10:50 +0900

    NumPy20本ノック1

---
+++ NumPy20.py
@@ -0,0 +1,13 @@
+#参考 https://www.youtube.com/watch?v=k4YzlaOXfvQ
+#NumPy20本ノック
+
+"""
+NumPy20本ノック1
+"""
+
+import numpy as np
+a = np.array([1,2,3,4])
+print(a)
+
+b = np.array([[1],[2],[3]]) 
+print(b)
