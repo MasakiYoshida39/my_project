f1969e5bafc4f6492d9bc8dca0ab1016ba5a0d52
MasakiYoshida39 <yashukasono@gmail.com>
2025-07-14 21:49:25 +0900

    Pandas20本ノック3

--- knock20/Pandas20.py
+++ knock20/Pandas20.py
@@ -4,6 +4,29 @@ https://www.youtube.com/watch?v=ZQZ38rK28Gk
 """
 import pandas as pd
 
+"""
+Pandas20本ノック3
+df=pd.read_csv('weather.csv')
+# カラムを表示
+df1 = df.columns
+print(df1)
+# 必要な部分のみ記載
+df1=df[['年月日', 
+        '平均気温(℃)', 
+        '平均気温(℃).1', 
+        '最低気温(℃)', 
+        '降水量の合計(mm)',
+        '最深積雪(cm)',
+       '平均雲量(10分比)',  
+       '平均蒸気圧(hPa)',
+       '平均風速(m/s)',
+        '日照時間(時間)']]
+
+df1=df1.head(2)
+print(df1)
+"""
+
+
 
 """
 Pandas20本ノック2
