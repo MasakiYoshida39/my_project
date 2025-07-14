53947d70cccfb8255bbab292186ab0be5a731c83
MasakiYoshida39 <yashukasono@gmail.com>
2025-07-14 21:38:04 +0900

    Pandas20本ノック2

--- knock20/Pandas20.py
+++ knock20/Pandas20.py
@@ -5,6 +5,17 @@ https://www.youtube.com/watch?v=ZQZ38rK28Gk
 import pandas as pd
 
 
+"""
+Pandas20本ノック2
+
+df=pd.read_csv('weather.csv')
+# head(3)＝先頭3行を表示
+df1=df.head(3)
+print(df1)
+# tail(10)＝後ろ10行を表示
+df1=df.tail(10)
+print(df1)
+"""
 
 """
 Pandas20本ノック１
