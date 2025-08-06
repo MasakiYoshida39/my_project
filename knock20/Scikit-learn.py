2ac45e9bb5603d588d033abf9f345574b2d9396d
MasakiYoshida39 <yashukasono@gmail.com>
2025-08-06 10:07:45 +0900

    Scikit-learn20本ノック１

---
+++ knock20/Scikit-learn.py
@@ -0,0 +1,16 @@
+"""
+参考URL
+https://www.youtube.com/watch?v=Fbynk_9TfXg
+"""
+
+"""
+Scikit-learn20本ノック
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from glob import glob
+
+df = pd.read_csv('data.csv')
+df.head()
